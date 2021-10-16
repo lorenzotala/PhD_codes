11d18f3409112cac4ef4fd3711bd9433618372ad
Lorenzo Talà <34740200+lorenzotala@users.noreply.github.com>
2021-10-16 13:42:23 +0200

    PaQa_measurments and TDMS conversion

--- Mechanosensation/Python_code/T8V_singaling/.ipynb_checkpoints/paqa_level_utils-checkpoint.py
+++ Mechanosensation/Python_code/T8V_singaling/.ipynb_checkpoints/paqa_level_utils-checkpoint.py
@@ -64,7 +64,7 @@ def combineDataSets(data_path, data_summary_path, working_path):
     combined_csv.to_csv( output_filename, index=False, encoding='utf-8-sig')
     return output_filename
 
-def plottingData(df, means, wt, order, strain_to_remove, growth, param, colors, scale, GraphTitle):
+def plottingData(df, means, wt, order, strain_to_remove, growth, param, colors, scale, GraphTitle, graph_size):
     df2 = df.copy()
     means2 = means.copy()
     if isinstance(growth, list):
@@ -165,8 +165,8 @@ def plottingData(df, means, wt, order, strain_to_remove, growth, param, colors, 
         lookupdf2=df3.groupby(['Strain', 'Growth', 'Labels', 'Bio_Rep'])[param].median().to_frame().reset_index()
         strain_mean = means3.groupby(['Strain', 'Growth', 'Labels'])[param].mean().to_frame().reset_index()
         p = bokeh.plotting.figure(
-            width=600, 
-            height=400, 
+            width=graph_size[0], 
+            height=graph_size[1], 
             y_range=np.flip(labels), 
             x_axis_type=scale,
             x_axis_label = 'Relative PaQa level',
@@ -271,8 +271,8 @@ def plottingData(df, means, wt, order, strain_to_remove, growth, param, colors, 
         lookupdf2=df3.groupby(['Strain', 'Growth', 'Labels', 'Bio_Rep'])[param].median().to_frame().reset_index()
         strain_mean = means3.groupby(['Strain', 'Growth', 'Labels'])[param].mean().to_frame().reset_index()
         p = bokeh.plotting.figure(
-            width=600, 
-            height=400, 
+            width=graph_size[0], 
+            height=graph_size[1],  
             y_range=labels, 
             x_axis_type=scale,
             x_axis_label = 'Relative PaQa level',
@@ -295,6 +295,292 @@ def plottingData(df, means, wt, order, strain_to_remove, growth, param, colors, 
             line_color = 'black',
             fill_color = 'white',
         )
+        p.circle(
+            source = strain_mean,
+            x = param,
+            y = 'Labels',
+            size = 10,
+            line_color = 'black',
+            fill_color = 'black',
+        )
+        p.add_tools(HoverTool(
+                tooltips=[
+                    ('Strain', '@{Strain}'),
+                    ('Growth condition', '@{Growth}'),
+                    (param, '@{'+param+'}'),
+                    ('Biological replicate', '@{Bio_Rep}')
+                ],
+        ))
+        p.line(
+            x = [1, 1],
+            y = [0, len(labels)],
+            line_width = 2,
+            line_color = 'red',
+            legend_label = "Reference"
+        )
+    return p, means3, df3
+
+def plottingBioReps(df, means, wt, order, strain_to_remove, growth, param, colors, scale, GraphTitle, graph_size):
+    df2 = df.copy()
+    means2 = means.copy()
+    if isinstance(growth, list):
+        #Removing unwanted strains from plot
+        if len(strain_to_remove) > 0:
+            for strain in strain_to_remove:
+                df2 = df2.drop(df2[(df2['Strain'] == strain)].index)
+                means2 = means2.drop(means2[(means2['Strain'] == strain)].index)
+        #Generate a new DataFrame for the filtered dataset
+        df3=df2.copy()
+        means3=means2.copy()
+        #Initialization of data labels and condition indexes
+        labels = [None]*(2+len(order)*2)
+        indexes = [None]*(2+len(order)*2)
+        n=0
+        #Extracting data for the user-defined WT strain on liquid
+        WT_mean=np.array(means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Liquid')), param])
+        if WT_mean.any():
+            Norm_mean=WT_mean/np.mean(WT_mean)
+            means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Liquid')), param]=Norm_mean
+            df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Liquid')), param] = df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Liquid')), param]/np.mean(WT_mean)
+            if '-' in wt:
+                strain_name=wt.replace('-', '')
+                labels[n] = chr(916)+strain_name + ' ' + 'Liquid'
+            else:
+                strain_name=wt
+                labels[n] = strain_name + ' ' + 'Liquid'
+            indexes[n] = ((df3['Strain'] == (wt)) & df3['Growth'].str.match('Liquid'))
+            print(wt+' Liquid mean='+str(WT_mean))
+            n=n+1
+        #Extracting data for the user-defined WT strain on Solid
+        Strain_mean=np.array(means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid')), param])
+        if Strain_mean.any():
+            Norm_mean=Strain_mean/np.mean(WT_mean)
+            means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid')), param]=Norm_mean
+            df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Solid')), param] = df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Solid')), param]/np.mean(WT_mean)
+            if '-' in wt:
+                strain_name=wt.replace('-', '')
+                labels[n] = chr(916)+strain_name + ' ' + 'Solid'
+            else:
+                strain_name=wt
+                labels[n] = strain_name + ' ' + 'Solid'
+            indexes[n] = ((df3['Strain'] == (wt)) & df3['Growth'].str.match('Solid'))
+            print(wt+' Solid mean='+str(Strain_mean))
+            print(wt+' Solid ref fold increase = '+str(Strain_mean/np.mean(WT_mean)) + ', mean = ' + str(np.mean(Strain_mean/np.mean(WT_mean))))
+            ref_fold = np.divide(Strain_mean, WT_mean)
+            print(wt+' Solid biorep fold increase='+str(ref_fold) + ', mean = ' + str(np.mean(ref_fold)))
+            n=n+1
+        #Extracting data for the other selected strains
+        for o, ordering in enumerate(order):
+            for a, g in enumerate(growth):
+                #Check if data is acquired for the specific growth condition
+                Strain_mean=np.array(means3.loc[((means3['Strain'] == (ordering)) & means3['Growth'].str.match(g)), param])
+                if Strain_mean.any():
+                    #Normalize each biological mean by the WT liquid mean and store it in the new means3 dataframe
+                    Norm_mean=Strain_mean/np.mean(WT_mean)
+                    means3.loc[((means3['Strain'] == (ordering)) & means3['Growth'].str.match(g)), param]=Norm_mean
+                    print(ordering+' '+g+' mean='+str(Strain_mean))
+                    print(ordering+' '+g+' ref fold increase = '+str(Strain_mean/np.mean(WT_mean)) + ', mean = ' + str(np.mean(Strain_mean/np.mean(WT_mean))))
+                    if len(WT_mean)==len(Strain_mean):
+                        ref_fold = np.divide(Strain_mean, WT_mean)
+                        print(ordering+' '+g+' biorep fold increase='+str(ref_fold) + ', mean = ' + str(np.mean(ref_fold)))
+                    #Normalize each datapoint by the WT liquid mean
+                    df3.loc[((df2['Strain'] == (ordering)) & df3['Growth'].str.match(g)), param] = df3.loc[((df2['Strain'] == (ordering)) & df3['Growth'].str.match(g)), param]/np.mean(WT_mean)
+                    #Recording labels of dataset
+                    if '-' in ordering:
+                        strain_name=ordering.replace('-', '')
+                        labels[n] = chr(916)+strain_name + ' ' +g
+                    else:
+                        strain_name=ordering
+                        labels[n] = strain_name + ' ' +g
+                    #Recording the indexes of the dataframe corresponding to the correct strain and growth condition
+                    indexes[n] = ((df3['Strain'] == (ordering)) & df3['Growth'].str.match(g))
+                    n=n+1
+        #Shorten the labels and indexes variables by removing all 'None' values related to the initialization
+        labels = list(filter(None, labels))
+        indexes = [i for i in indexes if not isinstance(i, type(None))]
+        #Setting the ploting order of the WT liquid and solid datasets
+        val = len(labels) - 0.5
+        if WT_mean.any():
+            df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Liquid')), 'Labels'] = val
+            means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Liquid')), 'Labels'] = val
+            val = val - 1
+        Strain_mean=np.array(means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid')), param])
+        if Strain_mean.any():
+            df3.loc[((df2['Strain'] == (wt)) & df3['Growth'].str.match('Solid')), 'Labels'] = val
+            means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid')), 'Labels'] = val
+            val = val - 1
+        #Setting the plotting order of the selected strains
+        for o, ordering in enumerate(order):
+            for a, g in enumerate(growth):
+                #Check if data is acquired for the specific growth condition
+                Strain_mean=np.array(means3.loc[((means3['Strain'] == (ordering)) & means3['Growth'].str.match(g)), param])
+                if Strain_mean.any():
+                    df3.loc[((df2['Strain'] == (ordering)) & df3['Growth'].str.match(g)), 'Labels'] = val
+                    means3.loc[((means3['Strain'] == (ordering)) & means3['Growth'].str.match(g)), 'Labels'] = val
+                    val = val - 1
+        lookupdf2=df3.groupby(['Strain', 'Growth', 'Labels', 'Bio_Rep'])[param].median().to_frame().reset_index()
+        strain_mean = means3.groupby(['Strain', 'Growth', 'Labels'])[param].mean().to_frame().reset_index()
+        p = bokeh.plotting.figure(
+            width=graph_size[0], 
+            height=graph_size[1], 
+            y_range=np.flip(labels), 
+            x_axis_type=scale,
+            x_axis_label = 'Relative PaQa level',
+            title=GraphTitle+" "+param+" on "+growth[0]+" vs. "+growth[1]+" in "+scale+" scale"
+        )
+        p.yaxis.major_label_text_font_size = "9pt"
+        p.circle(
+            source = means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Liquid'))],
+            x = param,
+            y = 'Labels',
+            size = 10,
+            line_color = 'black',
+            fill_color = colors[0]
+        )
+        p.line(
+            x = [np.mean(strain_mean.loc[((strain_mean['Strain'] == (wt)) & strain_mean['Growth'].str.match('Liquid')), 'PaQa_ratio']), np.mean(strain_mean.loc[((strain_mean['Strain'] == (wt)) & strain_mean['Growth'].str.match('Liquid')), 'PaQa_ratio'])],
+            y = [np.mean(strain_mean.loc[((strain_mean['Strain'] == (wt)) & strain_mean['Growth'].str.match('Liquid')), 'Labels'])-0.2, np.mean(strain_mean.loc[((strain_mean['Strain'] == (wt)) & strain_mean['Growth'].str.match('Liquid')), 'Labels'])+0.2],
+            line_width = 3,
+            line_color = 'black',
+            legend_label = "mean"
+        )
+        p.circle(
+            source = means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid'))],
+            x = param,
+            y = 'Labels',
+            size = 10,
+            line_color = 'black',
+            fill_color = colors[1]
+        )
+        p.line(
+            x = [np.mean(strain_mean.loc[((strain_mean['Strain'] == (wt)) & strain_mean['Growth'].str.match('Solid')), 'PaQa_ratio']), np.mean(strain_mean.loc[((strain_mean['Strain'] == (wt)) & strain_mean['Growth'].str.match('Solid')), 'PaQa_ratio'])],
+            y = [np.mean(strain_mean.loc[((strain_mean['Strain'] == (wt)) & strain_mean['Growth'].str.match('Solid')), 'Labels'])-0.2, np.mean(strain_mean.loc[((strain_mean['Strain'] == (wt)) & strain_mean['Growth'].str.match('Solid')), 'Labels'])+0.2],
+            line_width = 3,
+            line_color = 'black',
+        )
+        for o, ordering in enumerate(order):
+            p.circle(
+                source = means3.loc[((means3['Strain'] == ordering) & means3['Growth'].str.match('Liquid'))],
+                x = param,
+                y = 'Labels',
+                size = 10,
+                line_color = 'black',
+                fill_color = colors[o+2],
+                legend_label = "means of replicates"
+            )
+            p.line(
+                x = [np.mean(strain_mean.loc[((strain_mean['Strain'] == ordering) & strain_mean['Growth'].str.match('Liquid')), 'PaQa_ratio']), np.mean(strain_mean.loc[((strain_mean['Strain'] == ordering) & strain_mean['Growth'].str.match('Liquid')), 'PaQa_ratio'])],
+                y = [np.mean(strain_mean.loc[((strain_mean['Strain'] == ordering) & strain_mean['Growth'].str.match('Liquid')), 'Labels'])-0.2, np.mean(strain_mean.loc[((strain_mean['Strain'] == ordering) & strain_mean['Growth'].str.match('Liquid')), 'Labels'])+0.2],
+                line_width = 3,
+                line_color = 'black'
+            )
+        p.add_tools(HoverTool(
+                tooltips=[
+                    ('Strain', '@{Strain}'),
+                    ('Growth condition', '@{Growth}'),
+                    (param, '@{'+param+'}'),
+                    ('Biological replicate', '@{Bio_Rep}')
+                ],
+        ))
+        p.line(
+            x = [1, 1],
+            y = [0, len(labels)],
+            line_width = 1,
+            line_color = 'black',
+            line_dash = 'dotted',
+            legend_label = "Reference"
+        )
+    else:
+        print('Not a list')
+        if growth == 'Liquid':
+            ungrowth = 'Solid'
+        else:
+            ungrowth = 'Liquid'
+        if len(strain_to_remove) > 0:
+            for strain in strain_to_remove:
+                df2 = df2.drop(df2[(df2['Strain'] == strain)].index)
+                means2 = means2.drop(means2[(means2['Strain'] == strain)].index)    
+        for strain in order:
+            df3 = df2.drop(df2[((df2['Strain'] == strain) & (df2['Growth'] == ungrowth))].index)
+            means2 = means2.drop(means2[((means2['Strain'] == strain) & (means2['Growth'] == ungrowth))].index)
+        means3=means2
+        val = 0.5+len(order)+1
+        labels = [None]*(2+len(order))
+        indexes = [None]*(2+len(order))
+        df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Liquid')), 'Labels'] = val
+        means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Liquid')), 'Labels'] = val
+        WT_mean=np.array(means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Liquid')), param])
+        Norm_mean=WT_mean/np.mean(WT_mean)
+        means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Liquid')), param]=Norm_mean
+        df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Liquid')), param] = df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Liquid')), param]/np.mean(WT_mean)
+        labels[len(labels)-1] = 'WT'+' '+'Liquid'
+        indexes[0] = ((df3['Strain'] == (wt)) & df3['Growth'].str.match('Liquid'))
+        print(wt+' Liquid mean='+str(WT_mean))
+        df3.loc[((df2['Strain'] == (wt)) & df3['Growth'].str.match('Solid')), 'Labels'] = (val-1)
+        means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid')), 'Labels'] = (val-1)
+        Strain_mean=np.array(means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid')), param])
+        Norm_mean=Strain_mean/np.mean(WT_mean)
+        means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid')), param]=Norm_mean
+        df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Solid')), param] = df3.loc[((df3['Strain'] == (wt)) & df3['Growth'].str.match('Solid')), param]/np.mean(WT_mean)
+        labels[len(labels)-2] = 'WT'+' '+'Solid'
+        indexes[1] = ((df3['Strain'] == (wt)) & df3['Growth'].str.match('Solid'))
+        print(wt+' Solid mean='+str(Strain_mean))
+        print(wt+' Solid fold increase='+str(Strain_mean/np.mean(WT_mean)))
+        val = val - 2
+        for o, ordering in enumerate(order):
+            df3.loc[((df2['Strain'] == (ordering)) & df3['Growth'].str.match(growth)), 'Labels'] = val
+            means3.loc[((means3['Strain'] == (ordering)) & means3['Growth'].str.match(growth)), 'Labels'] = val
+            Strain_mean=np.array(means3.loc[((means3['Strain'] == (ordering)) & means3['Growth'].str.match(growth)), param])
+            Norm_mean=Strain_mean/np.mean(WT_mean)
+            means3.loc[((means3['Strain'] == (ordering)) & means3['Growth'].str.match(growth)), param]=Norm_mean
+            print(ordering+' mean='+str(Strain_mean))
+            print(ordering+' fold increase='+str(Strain_mean/np.mean(WT_mean)))
+            df3.loc[((df2['Strain'] == (ordering)) & df3['Growth'].str.match(growth)), param] = df3.loc[((df2['Strain'] == (ordering)) & df3['Growth'].str.match(growth)), param]/np.mean(WT_mean)
+            if '-' in ordering:
+                strain_name=ordering.replace('-', '')
+                labels[len(labels)-3-o] = chr(916)+strain_name
+            else:
+                strain_name=ordering
+                labels[len(labels)-3-o] = strain_name
+            indexes[o+2] = ((df3['Strain'] == (ordering)) & df3['Growth'].str.match(growth))
+            val = val - 1
+        lookupdf2=df3.groupby(['Strain', 'Growth', 'Labels', 'Bio_Rep'])[param].median().to_frame().reset_index()
+        strain_mean = means3.groupby(['Strain', 'Growth', 'Labels'])[param].mean().to_frame().reset_index()
+        p = bokeh.plotting.figure(
+            width=graph_size[0], 
+            height=graph_size[1],  
+            y_range=labels, 
+            x_axis_type=scale,
+            x_axis_label = 'Relative PaQa level',
+            title=GraphTitle+" "+param+" on "+growth
+        )
+        p.yaxis.major_label_text_font_size = "9pt"
+        p.circle(
+            source = means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Liquid'))],
+            x = param,
+            y = 'Labels',
+            size = 10,
+            line_color = 'black',
+            fill_color = colors[0]
+        )
+        p.circle(
+            source = means3.loc[((means3['Strain'] == (wt)) & means3['Growth'].str.match('Solid'))],
+            x = param,
+            y = 'Labels',
+            size = 10,
+            line_color = 'black',
+            fill_color = colors[1]
+        )
+        for o, ordering in enumerate(order):
+            p.circle(
+            source = means3.loc[((means3['Strain'] == ordering) & means3['Growth'].str.match('Liquid'))],
+            x = param,
+            y = 'Labels',
+            size = 10,
+            line_color = 'black',
+            fill_color = colors[o+2],
+            legend_label = "means of replicates"
+            )
         p.circle(
             source = strain_mean,
             x = param,
